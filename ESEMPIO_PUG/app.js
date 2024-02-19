0c3c2c5171f6aab09ec6f45914563c829be3d48b
Vincenza-Genuardi <genuardi.vincenza@iisgalvanimi.edu.it>
2024-02-19 00:54:49 +0000

    pug seconda parte

--- ESEMPIO_PUG/app.js
+++ ESEMPIO_PUG/app.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
+const people = require('./people.json');
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public')); 
 
 app.get('/', function (req, res) {
-res.render('index');
+
+    res.render('index', {
+        title: 'Homepage',
+        people: people.profiles
+    });
 });
 app.listen(3000, function () {
 console.log('Example app listening on port 3000!');
